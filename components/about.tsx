98e33357d053fec1db176969be9a8f01d457078e
Hongjin Quan <hq48@cornell.edu>
2023-12-01 20:47:00 -0500

    feat: about page

--- components/about.tsx
+++ components/about.tsx
@@ -20,24 +20,24 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Chemical Engineering</span>, I decided to combine my
-        background with programming in Health Tech. I self-learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="font-medium text-blue-600">Chemical Engineering</span>, I decided to combine my
+        background with programming in <span className="text-blue-600 font-medium">Health Tech Software Development</span> . I self-learned{" "}
+        <span className="font-medium text-blue-600">full-stack web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
-        <span className="font-medium">
+        <span className="font-medium text-xl text-blue-600">
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I am also studying Docker and Kubernetes. I am always looking to
-        learn new technologies. I am currently looking for an{" "}
-        <span className="font-medium">internship</span> as a software
+        . I am also studying <span className="text-blue-600"> Docker and Kubernetes</span>. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="text-xl font-medium text-blue-600">2024 summer internship</span> as a software
         development engineer.
       </p>
 
       <p>
-        <span className="italic">When I'm not studying</span>, I enjoy watching movies, and playing with my cats, all of them are rescued stray cats during my undergrad.
+        <span className="italic">When I'm not studying</span>, I enjoy watching movies, and playing with my <span className="text-amber-500 text-xl">cats</span>, all of them are rescued stray cats during my undergrad.
       </p>
     </motion.section>
   );
